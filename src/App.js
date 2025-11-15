f5d7aaeb0a7367ebcecf2c6d63790f833b3309f1
Saijanumuri <srkbvm2002@gmail.com>
2025-11-15 15:05:46 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React  from "react";
+import { useState , useEffect} from "react";
+import axios from "axios";
+
 
 function App() {
+  let [data,setData]=useState([[]])
+  useEffect(()=>{
+    axios.get("https://xcountries-backend.labs.crio.do/all")
+    .then((res)=>{setData(res.data)})
+    .catch((err)=>{console.error("Error fetching data: ", err)})
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{
+        display: "flex",
+        flexWrap: "wrap",
+        gap: "20px",
+        justifyContent: "center",
+        padding: "30px",
+        background: "#f0f2f5",
+        minHeight: "100vh",
+      }}>
+      {data.map((country)=>(
+        <div   style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            gap: "10px",
+            background: "white",
+            height: "max-content",
+            width: "200px",
+            borderRadius: "15px",
+            boxShadow: "0px 4px 12px rgba(0, 0, 0, 0.15)",
+            textAlign: "center",
+            padding: "15px",
+            transition: "0.3s",
+            cursor: "pointer",
+          }}>
+          <img src={country.flag} alt={country.abbr} style={{
+              width: "120px",
+              height: "80px",
+              objectFit: "contain",
+              borderRadius: "8px",
+            }}/>
+          <h2>{country.name}</h2>
+        </div>
+      )
+      
+      )}
+
     </div>
+    
   );
 }
 
